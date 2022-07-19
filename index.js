f06d0849354eb3f7d0af16931f0892c8c56c34b3
Adán Rodríguez <adan.rodriguez.fusta@gmail.com>
2022-07-19 18:39:54 -0300

    :tada: Add index.js
    Resuelvo primer ejercicio JS

---
+++ index.js
@@ -0,0 +1,12 @@
+const myName = "Adán Rodríguez"; 
+const myAge = 26;
+const isDating = false;
+const numOneToTen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const nameFiveSkillers = ["Fabricio Hang", "Gonzalo Herrero", "Mathias Ezequiel Latronico", "Paloma Martinez", "Sofía Hojberg"];
+const myFirstObject = {
+    myName : "Adán Rodríguez",
+    myAge : 26,
+    isDating : false,
+    numOneToTen : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    nameFiveSkillers : ["Fabricio Hang", "Gonzalo Herrero", "Mathias Ezequiel Latronico", "Paloma Martinez", "Sofía Hojberg"]
+};
